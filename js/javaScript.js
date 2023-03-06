de288a80318daf821504d03243ddbd44be6a115d
lapepinasouwu <soyunpanconmantequilla77@gmail.com>
2023-03-06 03:16:18 -0600

    Completado

--- js/javaScript.js
+++ js/javaScript.js
@@ -13,6 +13,17 @@ import {
   GithubAuthProvider,
 } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
 
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  getDoc,
+  setDoc,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBHoyhZGGSA3XX2VkvPd-8_ZuCPAESVb7M",
@@ -33,12 +44,32 @@ const mail = document.getElementById("mail");
 const pass = document.getElementById("pass");
 const close = document.getElementById("close");
 const observer = document.getElementById("observer");
-const providerGoogle = new GoogleAuthProvider();
 const googlebut = document.getElementById("googlebut");
+const providerGoogle = new GoogleAuthProvider();
 const facebut = document.getElementById("facebut");
 const providerFacebook = new FacebookAuthProvider();
 const gitbut = document.getElementById("gitbut");
 const providerGit = new GithubAuthProvider();
+const principal = document.getElementById("principal");
+const crud = document.getElementById("crud");
+const nombre = document.getElementById("nombre");
+const usuario = document.getElementById("usuario");
+const pais = document.getElementById("pais");
+const add = document.getElementById("add");
+const leer = document.getElementById("leer");
+const actualizar = document.getElementById("actualizar");
+const borrar = document.getElementById("borrar");
+const mostrar = document.getElementById("mostrar");
+const id = document.getElementById("nombre2");
+const usuario2 = document.getElementById("usuario2");
+const pais2 = document.getElementById("pais2");
+const puntuacion = document.getElementById("puntuacion");
+const tablas = document.getElementById("tabla");
+
+var long;
+var lat;
+
+const db = getFirestore(app);
 
 reg.addEventListener("click", function () {
   createUserWithEmailAndPassword(auth, mail.value, pass.value)
@@ -89,14 +120,18 @@ onAuthStateChanged(auth, (user) => {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/firebase.User
     const uid = user.uid;
+    principal.classList.add("hide");
+    crud.classList.remove("hide");
     observer.innerHTML = `
-      Cuenta conectada: ${user.email}
+      ${user.email}
       `;
     // ...
   } else {
     // User is signed out
     // ...
-    observer.innerText = "No hay una cuenta conectada";
+    principal.classList.remove("hide");
+    crud.classList.add("hide");
+    observer.innerText = "No hay una cuenta";
   }
 });
 
@@ -172,4 +207,138 @@ gitbut.addEventListener("click", function () {
     });
 });
 
-//SS
+//Código del crud//
+
+add.addEventListener("click", async () => {
+  try {
+    await setDoc(doc(db, "users", nombre.value), {
+      Nombre: nombre.value,
+      Usuario: usuario.value,
+      País: pais.value,
+    });
+    alert(`Al id ${nombre.value} se le creó un registro.`);
+  } catch (error) {
+    alert("Llena los datos");
+    console.error("Error adding document: " + error);
+  }
+});
+
+leer.addEventListener("click", async () => {
+  const docRef = doc(db, "users", id.value);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    usuario2.value = docSnap.data().Usuario;
+    pais2.value = docSnap.data().País;
+    puntuacion.value = docSnap.data().Puntuación;
+    long = docSnap.data().Longitud;
+    lat = docSnap.data().Latitud;
+
+    console.log("Document data:", docSnap.data());
+    //MAPBOX
+
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoieWVndXNzc3MiLCJhIjoiY2xld2sxMHBlMjZmeDNzcTZtbHBrcjFpeSJ9.vfkeU_92PS5XbO76f9-UMw";
+    const map = new mapboxgl.Map({
+      container: "map",
+      // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [long, lat],
+      zoom: 2,
+      projection: "globe", // starting projection
+      // create the gl context with MSAA antialiasing, so custom layers are antialiased
+    });
+
+    // Add the control to the map.
+    map.addControl(
+      new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+      })
+    );
+
+    map.on("load", () => {
+      // Load an image from an external URL.
+      map.loadImage(
+        "https://img.icons8.com/emoji/256/black-cat-emoji.png",
+        (error, image) => {
+          if (error) throw error;
+
+          // Add the image to the map style.
+          map.addImage("cat", image);
+
+          // Add a data source containing one point feature.
+          map.addSource("point", {
+            type: "geojson",
+            data: {
+              type: "FeatureCollection",
+              features: [
+                {
+                  type: "Feature",
+                  geometry: {
+                    type: "Point",
+                    coordinates: [long, lat],
+                  },
+                },
+              ],
+            },
+          });
+
+          // Add a layer to use the image to represent the data.
+          map.addLayer({
+            id: "points",
+            type: "symbol",
+            source: "point", // reference the data source
+            layout: {
+              "icon-image": "cat", // reference the image
+              "icon-size": 0.25,
+            },
+          });
+        }
+      );
+    });
+  } else {
+    // doc.data() will be undefined in this case
+    console.log("No such document!");
+    alert("No hay registros, o el ID está mal.");
+  }
+});
+
+actualizar.addEventListener("click", async () => {
+  const elementRef = doc(db, "users", id.value);
+
+  await updateDoc(elementRef, {
+    Nombre: nombre2.value,
+    Usuario: usuario2.value,
+    País: pais2.value,
+    Puntuación: puntuacion.value,
+  });
+  alert("El registro de " + id.value + " han cambiado.");
+});
+
+borrar.addEventListener("click", async () => {
+  await deleteDoc(doc(db, "users", id.value));
+
+  alert("El registro de " + id.value + " se borró");
+});
+
+mostrar.addEventListener("click", async () => {
+  tablas.innerHTML = `<tr>
+      <td>  ID  </td>
+      <td>  Nombre  </td>
+      <td>  Usuario  </td>
+      <td>  País  </td>
+      <td>  Puntuación  </td>
+  </tr>`;
+  const querySnapshot = await getDocs(collection(db, "users"));
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, " => ", doc.data());
+    tablas.innerHTML += `<tr>
+          <td>${doc.id}</td>
+          <td>${doc.data().Nombre}</td>
+          <td>${doc.data().Usuario}</td>
+          <td>${doc.data().País}</td>
+          <td>${doc.data().Puntuación}</td>
+      </tr>`;
+  });
+});
